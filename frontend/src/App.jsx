3729fcde002645b21cd1adbe954f434987f46c1b
erbishwas <erbishwas@gmail.com>
2025-06-02 15:13:06 +0545

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+import Mainbody from './components/Mainbody.jsx'
+
+const App = () => {
+  return (
+    <>
+        <Navbar />
+        <Mainbody />
+        <Footer />
+    </>
+  )
+}
+
+export default App
